5cbe9053ad907ec1a2cd9361a2e457204292675a
JesseH44 <jdhoogs44@gmail.com>
2024-07-29 13:02:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 from dotenv import dotenv_values
 
+import utils
 from processors.deepforest_processor import DeepForestProcessor
 from processors.detectree_processor import DetecTreeProcessor
 from processors.samgeo_processor import SamgeoProcessor
@@ -64,8 +65,37 @@ async def process(
 
         return FileResponse(output_file.name)
 
-@app.post("/eval")
-async def eval(
-    image_data: Annotated[bytes, File()], processor_type: Annotated[str, Form()]
+
+@app.post("/evaluate")
+async def evaluate(
+    base_mask: Annotated[bytes, File()],
+        model_mask: Annotated[bytes | None, File()],
+        csv_file: Annotated[bytes | None, File()]
 ):
+    with NamedTemporaryFile(
+        suffix=".png", delete=False
+    ) as input_mask, NamedTemporaryFile(suffix=".png", delete=False) as output_mask:
+        input_filepath = Path(input_mask.name)
+        output_filepath = Path(output_mask.name)
+
+        logging.debug(f"input: {input_filepath}")
+        logging.debug(f"output: {output_filepath}")
+
+        logging.info("Writing image data to temporary file")
+        input_filepath.write_bytes(base_mask)
+        output_filepath.write_bytes(model_mask)
+
+        logging.info("Computing metrics")
+        if processor_type == "detectree":
+            utils.evaluation(input_filepath, output_filepath)
+        elif processor_type == "deepforest":
+            utils.evaluation(input_filepath, output_filepath)
+        elif processor_type == "samgeo":
+            utils.evaluation(input_filepath, output_filepath)
+        else:
+            logging.warning("processor_type not allowed")
+            raise HTTPException(
+                status_code=400,
+                detail="processor_type must be one of 'samgeo', 'detectree', or 'deepforest'",
+            )
     raise NotImplementedError()
