113213b38241c4f9d4986b706f7e4bd7c1159cb9
alextang <alxhtang@gmail.com>
2024-07-29 13:50:32 +1000

    formatting

--- tree_frontend.py
+++ tree_frontend.py
@@ -3,8 +3,10 @@ from PIL import Image
 import gradio as gr
 from deepforest import main
 import tempfile
+
 model = main.deepforest()
 model.use_release()
+
 
 def convert_rgba_to_rgb_and_save(image):
     if image.mode == "RGBA":
@@ -22,14 +24,15 @@ def deepforest_process(px):
 
     temp_path = convert_rgba_to_rgb_and_save(img)
 
-    processed_img =  model.predict_tile(temp_path, return_plot = True, patch_size=300,patch_overlap=0.25)
+    processed_img = model.predict_tile(temp_path, return_plot=True, patch_size=300, patch_overlap=0.25)
 
     os.remove(temp_path)
     return processed_img
 
+
 interface = gr.Interface(fn=deepforest_process,
-                     inputs=gr.Image(type="numpy", label="Upload Image"),
-                     outputs=gr.Image(label="Predicted Image"),
-                     )
+                         inputs=gr.Image(type="numpy", label="Upload Image"),
+                         outputs=gr.Image(label="Predicted Image"),
+                         )
 
 interface.launch()
