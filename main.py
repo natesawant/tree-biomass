a4d77a085a667eaea13bca23a2ee49eb44622a46
JesseH44 <jdhoogs44@gmail.com>
2024-07-29 14:17:27 +1000

    evaluation method

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from fastapi import FastAPI, File, Form, HTTPException
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from dotenv import dotenv_values
+from PIL import Image
 
+import bounding_box_mask
 import utils
 from processors.deepforest_processor import DeepForestProcessor
 from processors.detectree_processor import DetecTreeProcessor
@@ -35,10 +37,10 @@ async def root():
 
 @app.post("/process")
 async def process(
-    image_data: Annotated[bytes, File()], processor_type: Annotated[str, Form()]
+        image_data: Annotated[bytes, File()], processor_type: Annotated[str, Form()]
 ):
     with NamedTemporaryFile(
-        suffix=".jpg", delete=False
+            suffix=".jpg", delete=False
     ) as input_file, NamedTemporaryFile(suffix=".tif", delete=False) as output_file:
         input_filepath = Path(input_file.name)
         output_filepath = Path(output_file.name)
@@ -68,34 +70,36 @@ async def process(
 
 @app.post("/evaluate")
 async def evaluate(
-    base_mask: Annotated[bytes, File()],
-        model_mask: Annotated[bytes | None, File()],
-        csv_file: Annotated[bytes | None, File()]
+        base_mask: Annotated[bytes, File()],
+        model_mask: Annotated[bytes | None, File()] = None,
+        csv_file: Annotated[bytes | None, File()] = None
 ):
-    with NamedTemporaryFile(
-        suffix=".png", delete=False
-    ) as input_mask, NamedTemporaryFile(suffix=".png", delete=False) as output_mask:
+    with (NamedTemporaryFile(suffix=".png", delete=False) as input_mask,
+          NamedTemporaryFile(suffix=".png", delete=False) as model,
+          NamedTemporaryFile(suffix=".csv", delete=False) as boxes,
+          NamedTemporaryFile(suffix=".png", delete=False) as output_csv):
         input_filepath = Path(input_mask.name)
-        output_filepath = Path(output_mask.name)
+        model_filepath = Path(model.name)
+        csv_boxes = Path(boxes.name)
+        output_csv_filepath = Path(output_csv.name)
 
         logging.debug(f"input: {input_filepath}")
-        logging.debug(f"output: {output_filepath}")
+        logging.debug(f"output: {model_filepath}")
+        logging.debug(f"output: {csv_boxes}")
 
         logging.info("Writing image data to temporary file")
         input_filepath.write_bytes(base_mask)
-        output_filepath.write_bytes(model_mask)
+        model_filepath.write_bytes(model_mask)
+        csv_boxes.write_bytes(csv_file)
 
         logging.info("Computing metrics")
-        if processor_type == "detectree":
-            utils.evaluation(input_filepath, output_filepath)
-        elif processor_type == "deepforest":
-            utils.evaluation(input_filepath, output_filepath)
-        elif processor_type == "samgeo":
-            utils.evaluation(input_filepath, output_filepath)
+        if csv_file is not None:
+            width, height = bounding_box_mask.get_image_dimensions(input_filepath)
+            bounding_box_mask.create_image_from_csv(csv_boxes, output_csv_filepath, width, height)
+            metrics = utils.evaluation(input_filepath, output_csv_filepath)
         else:
-            logging.warning("processor_type not allowed")
-            raise HTTPException(
-                status_code=400,
-                detail="processor_type must be one of 'samgeo', 'detectree', or 'deepforest'",
-            )
-    raise NotImplementedError()
+            metrics = utils.evaluation(input_filepath, model_filepath)
+
+        accuracy, precision, recall, f1_score = metrics
+
+        return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1_score": f1_score}
